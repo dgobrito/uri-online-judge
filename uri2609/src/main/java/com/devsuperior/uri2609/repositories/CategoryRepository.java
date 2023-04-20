c57386a50f94ca97eebe5f94e512f8e0fd64f9e6
Dyego dos Santos Brito <dyego.eti@gmail.com>
2023-04-19 21:44:23 -0300

    final

--- uri2609/src/main/java/com/devsuperior/uri2609/repositories/CategoryRepository.java
+++ uri2609/src/main/java/com/devsuperior/uri2609/repositories/CategoryRepository.java
@@ -1,9 +1,22 @@
 package com.devsuperior.uri2609.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.devsuperior.uri2609.dto.CategorySumDTO;
 import com.devsuperior.uri2609.entities.Category;
+import com.devsuperior.uri2609.projections.CategorySumProjection;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
-
+	
+	@Query(value = "SELECT categories.name, SUM(products.amount) FROM categories "
+				 + "INNER JOIN products ON categories.id = products.id_categories "
+				 + "GROUP BY categories.name", nativeQuery = true)
+	List<CategorySumProjection> search1();
+	
+	@Query(value = "SELECT new com.devsuperior.uri2609.dto.CategorySumDTO(obj.category.name, SUM(obj.amount)) FROM Product obj "
+			 	 + "GROUP BY obj.category.name")
+	List<CategorySumDTO> search2();
 }
--- uri2621/src/main/java/com/devsuperior/uri2621/repositories/ProductRepository.java
+++ uri2621/src/main/java/com/devsuperior/uri2621/repositories/ProductRepository.java
@@ -1,9 +1,28 @@
 package com.devsuperior.uri2621.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+import com.devsuperior.uri2621.dto.ProductMinDTO;
 import com.devsuperior.uri2621.entities.Product;
+import com.devsuperior.uri2621.projections.ProductMinProjection;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
-
+	
+	@Query(value = "SELECT products.name FROM products "
+				 + "INNER JOIN providers ON products.id_providers = providers.id "
+				 + "WHERE "
+				 + "products.amount BETWEEN :amountMin AND :amountMax AND "
+				 + "UPPER(providers.name) LIKE CONCAT(:beginProviderName, '%')", nativeQuery = true)
+	List<ProductMinProjection> search1(Integer amountMin, Integer amountMax, String beginProviderName);
+	
+	@Query(value = "SELECT new com.devsuperior.uri2621.dto.ProductMinDTO(obj.name) FROM Product obj "
+				 + "WHERE "
+				 + "obj.amount BETWEEN :amountMin AND :amountMax AND "
+				 + "UPPER(obj.provider.name) LIKE :beginProviderName%")
+	List<ProductMinDTO> search2(Integer amountMin, Integer amountMax, String beginProviderName);	
 }
